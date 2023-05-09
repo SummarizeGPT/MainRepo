b1c21f527c5940c60480bb86d557abb2d329b191
Muneeb-F <m5farruk@uwaterloo.ca>
2023-05-09 18:39:40 -0400

    Updated Code

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import { Provider } from "react-redux";
+import { Provider } from "react-redux";
 
 import App from "./App";
-// import { store } from "./services/store";
+import { store } from "./services/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
+    <Provider store={store}>
       <App />
-    {/* </Provider> */}
+    </Provider>
   </React.StrictMode>
 );
